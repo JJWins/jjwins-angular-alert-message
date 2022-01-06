debf48c8e4ead96fe85615c9b7b4ebbdf974d020
JJWins <65803455+JJWins@users.noreply.github.com>
2022-01-06 12:12:32 +0530

    added css classes

--- src/lib/alert-messages.component.ts
+++ src/lib/alert-messages.component.ts
@@ -13,22 +13,22 @@ import { AlertMessagesService } from './alert-messages.service';
     `
     .grayOutDiv
     {
-        background-color: #333;
-        opacity: 0.7;
-        position: fixed;
-        top: 0px;
-        left: 0px;
-        height: 100%;
-        width: 100%;
-        overflow: hidden;
-        z-index: 9999;
+      background-color: #333;
+      opacity: 0.7;
+      position: fixed;
+      top: 0px;
+      left: 0px;
+      height: 100%;
+      width: 100%;
+      overflow: hidden;
+      z-index: 9999;
     }
     .message
     {
-        z-index: 10000;
-        position: relative;
-        display:inline-block;
-        padding: .5rem;
+      z-index: 10000;
+      position: relative;
+      display:inline-block;
+      padding: .5rem;
     }
     .messsageRightPadding{
       padding-right: .5rem;
@@ -38,6 +38,29 @@ import { AlertMessagesService } from './alert-messages.service';
       border-radius:5px;
       background:none;
       cursor: pointer;
+    }
+    .alerts-message {
+      border-radius: 12px;
+      background-color: white;
+      box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 11px 35px rgba(0,0,0,0.23);
+    }
+    .alerts-error {
+      border-radius: 12px;
+      color: white;
+      background-color: red;
+      box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 11px 35px rgba(0,0,0,0.23);
+    }
+    .alerts-warning {
+      border-radius: 12px;
+      color: white;
+      background-color: #bf7100;
+      box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 11px 35px rgba(0,0,0,0.23);
+    }
+    .alerts-success {
+      border-radius: 12px;
+      color: white;
+      background-color: #00bf71;
+      box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 11px 35px rgba(0,0,0,0.23);
     }
     `
   ]
